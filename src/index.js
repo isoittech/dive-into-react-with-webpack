b7213e564390c44ccb8600ceb5e088dfe80741b1
t.i <isoittech@gmail.com>
2020-04-04 13:51:00 +0900

    modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import _ from 'lodash'
+// import { Nijou, NAME } from './utilities'
+// console.log(Nijou(17));
+// console.log(NAME);
 
-function component(){
+// import * as utilities from './utilities'
+// console.log(utilities.Nijou(17));
+// console.log(utilities.NAME + '君');
+
+import { Nijou as funcNijou, NAME as NAME_as_BOSE } from './utilities'
+console.log(funcNijou(17));
+console.log(NAME_as_BOSE + 'さん');
+
+import Lion from './utilities'
+console.log(Lion.say());
+
+import Tiger from './utilities'
+console.log('Tigarだぞ' + Tiger.say())
+
+
+function component() {
     const element = document.createElement('div');
     const array = ['Helloooooo', 'WebPAAAAACKKKKK', 'NNNNNIIIIIICEEEEEEEEE!!!'];
     element.innerHTML = _.join(array, ' ')
