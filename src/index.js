128682ab83bf19bf52ed5b8b19cb180343624fd1
t.i <isoittech@gmail.com>
2020-04-04 13:10:05 +0900

    webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash'
 
 function component(){
     const element = document.createElement('div');
-    const array = ['Helloooooo', 'WebPAAAAACKKKKK', 'Fuuuuuuuuuuuuucccccckkkkkk!!!'];
+    const array = ['Helloooooo', 'WebPAAAAACKKKKK', 'NNNNNIIIIIICEEEEEEEEE!!!'];
     element.innerHTML = _.join(array, ' ')
     return element;
 }
