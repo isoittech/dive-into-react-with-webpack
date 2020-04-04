a1acb0a6fdf4344a83cdc38f673fce82026aeee7
t.i <isoittech@gmail.com>
2020-04-04 19:04:34 +0900

    mini css extract plugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './style.css' // cssのhaikeiクラスをimportするため
+import './style.scss' // scssのdivタグのスタイルをimportするため
 
 ReactDOM.render(
     <div>Helloooooooooo, ReeeeaAAAAAccckTTTtttu !!!</div>,
