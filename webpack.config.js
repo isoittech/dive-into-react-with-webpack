14a0bc298595f117bac6a2feb762f15d0e98fe08
t.i <isoittech@gmail.com>
2020-04-04 14:57:30 +0900

    css-loader-and-style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,22 @@ module.exports = {
         // 名前を何にするのか
         filename: 'main.js'
     },
+    module: {
+        rules: [
+            {
+                // どういったルールで登録するのか。正規表現で書く
+                test: /\.css$/,
+                // 何を登録し適用させるのか
+                // 下記に書くものは順序に注意が必要である。
+                // 並び順とは逆順に評価・実行される。
+                // つまり下記は、css-loader、style-loaderの順序で実行される。
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
     devServer: {
         contentBase: outputPath
     }
